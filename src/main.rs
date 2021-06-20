7e6ce3799d7c97809df9e57c1cb8160fa74a2ca8
Ertugrul Karademir <ekarademir@gmail.com>
2021-06-20 10:39:41 +0100

    Implemented new

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,94 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-
 use log::{debug, info};
+use winit::window::Window;
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    async fn new(window: &Window) -> Result<Self, Box<dyn std::error::Error>> {
+        let size = window.inner_size();
+
+        // instance holds the handle to the GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU  (they are all ORed)
+        // TODO: Try BackendBit::VULKAN
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+
+        // This is unsafe because on some Linux systems lifetime of the window might not be as long
+        // as the lifetime of the program. See: https://github.com/gfx-rs/wgpu/issues/1463
+        let surface = unsafe { instance.create_surface(window) };
+
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+            }
+        ).await.expect("Can't initialize adapter with the surface.");
+
+        let format = adapter.get_swap_chain_preferred_format(&surface).expect(
+            "Can't get surface prefered texture format."
+        );
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                // Features are the capabilities of the API and the GPU
+                // They are not universal.
+                // See all features here: https://docs.rs/wgpu/0.7.0/wgpu/struct.Features.html
+                features: wgpu::Features::empty(),
+                // Limits are resource limits that can be imposed.
+                // They are device dependent
+                // See all limits here: https://docs.rs/wgpu/0.7.0/wgpu/struct.Limits.html
+                limits: wgpu::Limits::default(),
+                label: None,  // Debug label for the device
+            },
+            None, // Trace path used for tracing API calls if `trace` features is enabled.
+        ).await?;
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,  // Framerate will be capped with `VSync` frequency
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Ok(
+            Self {
+                surface,
+                device,
+                queue,
+                sc_desc,
+                swap_chain,
+                size,
+            }
+        )
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        todo!()
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
@@ -35,8 +121,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             _ => {}
         }
     );
-
-    Ok(())
 }
 
 enum ExitReason {
