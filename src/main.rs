65cca54ca57700700df67e5fc52a6530c9766757
Ertugrul Karademir <ekarademir@gmail.com>
2021-06-26 22:06:54 +0100

    Buffers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -5,6 +6,55 @@ use winit::{
 };
 use log::{debug, info, error};
 use winit::window::Window;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ]
+        }
+    }
+}
+
+// main.rs
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [1.0, 0.0, 0.0] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.0, 0.0, 0.0] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [1.0, 1.0, 1.0] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.0, 0.0, 1.0] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0],color: [0.0, 1.0, 0.0] }, // E
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+    // WGPU requires 4 bytes buffer alignment (packing)
+    // Above there are 9 u16 numbers which is 9 x 2 bytes
+    // We add one more u16 to square this
+    /* padding */ 0,
+];
+
+
 
 struct State {
     surface: wgpu::Surface,
@@ -15,6 +65,9 @@ struct State {
     size: winit::dpi::PhysicalSize<u32>,
     mouse_pos: cgmath::Point2<f64>,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 impl State {
@@ -83,7 +136,7 @@ impl State {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "main",
-                buffers: &[],  // This is defined in the shader for now
+                buffers: &[Vertex::desc()],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
@@ -114,6 +167,23 @@ impl State {
             },
         });
 
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsage::VERTEX,
+            }
+        );
+
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsage::INDEX,
+            }
+        );
+
+        let num_indices = INDICES.len() as u32;
 
         Ok(
             Self {
@@ -125,6 +195,9 @@ impl State {
                 size,
                 mouse_pos: cgmath::Point2 {x: 0.0, y: 0.0},
                 render_pipeline,
+                vertex_buffer,
+                index_buffer,
+                num_indices,
             }
         )
     }
@@ -187,10 +260,12 @@ impl State {
             );
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(
-                // [[builtin(vertex_index)]] comes from here
-                0..3,
-                0..1,
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(
+                0..self.num_indices,
+                0,
+                0..1
             );
         }
 
